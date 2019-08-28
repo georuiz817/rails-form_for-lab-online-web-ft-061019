553221a5d697df5e0eb4d8be0930b03b098af6fe
George Ruiz <georuiz817@gmail.com>
2019-08-28 20:20:37 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,1 +1,1 @@
-class SchoolClassesController < ApplicationControllerend
+class SchoolClassesController < ApplicationControllerdef new 	  @school_class = SchoolClass.new	end	def create 	@school_class = SchoolClass.new(class_params(:title,:room_number))  @school_class.save	redirect_to school_class_path(@school_class)	end     privatedef class_params(*args)   params.require(:school_class).permit(*args)endend
